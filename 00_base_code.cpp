a213fd7b3236466c4bb8da8eb1d4ff7db5f966ea
nishidate-yuki <asta18425@gmail.com>
2020-12-30 23:40:29 +0900

    update 00

--- 00_base_code.cpp
+++ 00_base_code.cpp
@@ -1,6 +1,9 @@
 
 #include "vkutils.hpp"
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class Application
 {
 public:
@@ -13,10 +16,16 @@ public:
     }
 
 private:
+    GLFWwindow* window;
 
     void initWindow()
     {
+        glfwInit();
 
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
     void initVulkan()
@@ -24,17 +33,18 @@ private:
 
     }
 
-
     void mainLoop()
     {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
-
 };
 
 int main()
