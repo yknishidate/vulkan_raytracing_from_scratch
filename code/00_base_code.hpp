da60de803c621fe059ba3c97e15b2fb86a5955f0
nishidate-yuki <yuki18425@gmail.com>
2024-01-02 17:45:02 +0900

    update 00

--- code/00_base_code.hpp
+++ code/00_base_code.hpp
@@ -4,45 +4,29 @@
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
-class Application
-{
+class Application {
 public:
-    void run()
-    {
+    void run() {
         initWindow();
         initVulkan();
-        mainLoop();
-        cleanup();
-    }
 
-private:
-    GLFWwindow* window;
-
-    void initWindow()
-    {
-        glfwInit();
-
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
-
-    void initVulkan()
-    {
-
-    }
-
-    void mainLoop()
-    {
         while (!glfwWindowShouldClose(window)) {
             glfwPollEvents();
         }
-    }
 
-    void cleanup()
-    {
         glfwDestroyWindow(window);
         glfwTerminate();
     }
+
+private:
+    GLFWwindow* window = nullptr;
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {}
 };
